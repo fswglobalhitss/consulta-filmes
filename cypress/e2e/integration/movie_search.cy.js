8f342b7110cb67ce87a8a517b3c8e06ba447f996
Bruno Rufino <bj_rufino@outlook.com>
2024-08-16 11:32:58 -0300

    subindo testes atualizados

--- cypress/e2e/integration/movie_search.cy.js
+++ cypress/e2e/integration/movie_search.cy.js
@@ -2,6 +2,7 @@ const userName = 'Pedro';
 const movieName = 'The Hobbit Series';
 const userName2 = 'Jane Doe';
 const movieNameNotFound = 'NonexistentMovie123';
+
 const url = 'http://localhost/'
 const elementNome = 'input[aria-label="nome"]';
 const elementMovie = 'input[aria-label="filme"]'; 
@@ -10,20 +11,27 @@ const elementMovie = 'input[aria-label="filme"]';
 describe('Movie Search', () => {
   it('should allow a user to search for a movie', () => {
     cy.visit(url);
+    // Preencher campos de busca
     cy.get(elementNome).type(userName);
     cy.get(elementMovie).type(movieName);
+    // Clicar no botão de pesquisa
     cy.get('button[ng-click="pesquisar();"]').click();
-    cy.get('#search-results').should('be.visible');
-    cy.get('#search-results').should('contain', movieName);
+    // Verificar se o <tbody> com os resultados está visível
+    cy.get('tbody').should('be.visible');
+    // Verificar se o nome do filme aparece nas células <td>
+    cy.get('tbody .ng-binding').should('contain', movieName);
   });
 
   it('should display an error message for invalid searches', () => {
     cy.visit(url);
+    cy.window().then(win => {
+      // Substituir a função alert global
+      cy.stub(win, 'alert').as('alertStub');
+    });
     cy.get(elementNome).type(userName2);
     cy.get(elementMovie).type(movieNameNotFound);
     cy.get('button[ng-click="pesquisar();"]').click();
-    cy.get('#error-message').should('be.visible');
-    cy.get('#error-message').should('contain', 'No results found');
+    cy.get('@alertStub').should('be.calledWith', 'Não foram encontrados filmes com os dados informados');
   });
 
   it('should display an error message for input name and film is empty', () => {
@@ -36,12 +44,15 @@ describe('Movie Search', () => {
 
     cy.get(elementNome).type(' ');
     cy.get(elementMovie).type(' ');
-    cy.get('button[ng-click="pesquisar();"]').click();  
+    cy.get('button[ng-click="pesquisar();"]').click();
+  
     cy.wait(2000);
     
    cy.screenshot('nome-da-captura');
    cy.get('@alertStub').should('be.calledWith', 'É necessário informar seu nome');
   });
+
+
 
   it('should display an error message for input name is empty', () => {
     var alertStub = cy.stub();
@@ -50,7 +61,8 @@ describe('Movie Search', () => {
       // Substituir a função alert global
       cy.stub(win, 'alert').as('alertStub');
     });
-    
+
+
     cy.get(elementNome).type(' ');
     cy.get(elementMovie).type(movieNameNotFound);
     cy.get('button[ng-click="pesquisar();"]').click();
@@ -72,8 +84,11 @@ describe('Movie Search', () => {
     cy.get(elementNome).type(userName2);
     cy.get(elementMovie).type(' ');
     cy.get('button[ng-click="pesquisar();"]').click();
+
     cy.wait(2000);
     cy.screenshot('nome-da-captura2');
     cy.get('@alertStub').should('be.calledWith', 'É necessário informar o nome do filme');
   });
+
+
 });
--- cypress/e2e/integration/search_history.cy.js
+++ cypress/e2e/integration/search_history.cy.js
@@ -8,7 +8,7 @@ const opcaoHistorico = '#historico-tab';
 const elementPesquisador = 'input[aria-label="pesquisador"]';
 const elementMovie = 'input[aria-label="nomeFilme"]'; 
 const elementBotao = 'button[ng-click="carregarHistorico();"]';
-
+const movieNameNotFound = 'Nao tem';
 
 const elementosTabela = ''
 
@@ -19,31 +19,7 @@ describe('Search History', () => {
     cy.get(elementPesquisador).should('be.visible');
     cy.get(elementMovie).should('be.visible');
     cy.get(elementBotao).should('be.visible');
-
-   // cy.get('div[id="historico-tab-pane"] table').within(() => {
-     // cy.get('tbody tr').each(($row, rowIndex) => {
-        // Obtenha os valores das células para cada coluna
-      //  cy.wrap($row).find('td').then($cells => {
-      //    const id = $cells.eq(0).text().trim();
-      //    const dataPesquisa = $cells.eq(1).text().trim();
-      //    const nomePesquisador = $cells.eq(2).text().trim();
-      //   const nomeFilme = $cells.eq(3).text().trim();
-
-          // Imprima os valores para verificação
-      /*    cy.log(`Linha ${rowIndex + 1}: Id=${id}, Data da Pesquisa=${dataPesquisa}, Nome do Pesquisador=${nomePesquisador}, Nome do Filme=${nomeFilme}`);
-
-          // Comparações com valores esperados (substitua pelos valores reais esperados)
-          // Exemplo:
-          expect(id).to.match(/^\d+$/); // Verifica se o Id é um número
-          expect(dataPesquisa).to.match(/^\d{2}\/\d{2}\/\d{4}$/); // Verifica o formato da data (dd/mm/aaaa)
-          expect(nomePesquisador).not.to.be.empty; // Verifica se o nome do pesquisador não está vazio
-          expect(nomeFilme).to.include('Alguma Parte do Nome do Filme'); // Verifica se o nome do filme contém uma string específica*/
-        });
-    //  });
-  //  });
- 
-
- // });
+  });
 
   it('should update history after a new search', () => {
     cy.visit(url);
@@ -51,29 +27,36 @@ describe('Search History', () => {
     cy.get(elementPesquisador).type(pesquisador);
     cy.get(elementMovie).type(movieName);
     cy.get(elementBotao).click();
-    cy.get('#search-results').should('be.visible');
-    cy.get('#search-results').should('contain', movieName);
+    cy.get('tbody').should('be.visible');
+    // Verificar se o nome do filme aparece nas células <td>
+    cy.get('tbody .ng-binding').should('contain', movieName);
   });
 
   it('should update history after a new search with user not found', () => {
     cy.visit(url);
+    cy.window().then(win => {
+      // Substituir a função alert global
+      cy.stub(win, 'alert').as('alertStub');
+    });
     cy.get(opcaoHistorico).click();
     cy.get(elementPesquisador).type(pesquisadorNaoExistente);
     cy.get(elementMovie).type(movieName);
     cy.get(elementBotao).click();
-   // cy.get('#error-message').should('be.visible');
-   // cy.get('#error-message').should('contain', 'No results found');
+    cy.get('@alertStub').should('be.calledWith', 'Não foi encontrado histórico com os dados informados');
   });
 
-  it('should update history after a new search with film not found', () => {
 
+  it('should update history after a new search with film not found', () => {
     cy.visit(url);
+    cy.window().then(win => {
+      // Substituir a função alert global
+      cy.stub(win, 'alert').as('alertStub');
+    });
     cy.get(opcaoHistorico).click();
     cy.get(elementPesquisador).type(pesquisador);
     cy.get(elementMovie).type(historicoNaoExistente);
     cy.get(elementBotao).click();
-
-   // cy.get('#error-message').should('be.visible');
-   // cy.get('#error-message').should('contain', 'No results found');;
+    cy.get(elementBotao).click();
+    cy.get('@alertStub').should('be.calledWith', 'Não foi encontrado histórico com os dados informados');
   });
 });
